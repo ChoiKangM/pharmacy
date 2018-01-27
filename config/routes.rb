dab1f512e58b83e5243d3bd5da9c712ceaa22913
EC2 Default User <ec2-user@ip-172-31-2-155.ap-southeast-1.compute.internal>
2018-01-27 02:35:30 +0000

    introduction#index

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   
   devise_for :users
   get '/introductions/user_information', to: 'introductions#user_information', as: 'user_information'
-  root 'introductions#hello'
+  root 'introductions#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
