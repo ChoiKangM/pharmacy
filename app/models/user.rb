d98d4530f49091091b573faaab806ed91a8a4acf
EC2 Default User <ec2-user@ip-172-31-2-155.ap-southeast-1.compute.internal>
2018-01-28 14:12:54 +0000

    omniauth4

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,20 @@ class User < ActiveRecord::Base
   validates_integrity_of  :user_face
   validates_processing_of :user_face
   validates :nickname, presence: true
+  # Omniauth 2.0
+  def self.from_omniauth(access_token)
+    data = access_token.info
+    user = User.where(email: data['email']).first
+
+    # Uncomment the section below if you want users to be created if they don't exist
+    unless user
+        user = User.create(name: data['name'],
+            email: data['email'],
+            password: Devise.friendly_token[0,20]
+        )
+    end
+    user
+  end
   private
     def user_face_size_validation
       errors[:user_face] << "should be less than 500KB" if user_face.size > 0.5.megabytes
