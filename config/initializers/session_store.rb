7d0ba5c1f1a1f9b9a5bed4637564b4689c2e45ba
EC2 Default User <ec2-user@ip-172-31-2-155.ap-southeast-1.compute.internal>
2018-01-28 14:36:48 +0000

    omniauth7

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_app_session', domain: 'http://fffff.knup.co.kr'
+Rails.application.config.session_store :cookie_store, key: '_workspace_session', domain: 'localhost'
