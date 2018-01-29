88b044f82fd35c983abc63a48804fdde461d6e70
EC2 Default User <ec2-user@ip-172-31-2-155.ap-southeast-1.compute.internal>
2018-01-29 05:00:08 +0000

    omniauth20

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -9,7 +9,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       # You need to implement the method below in your model (e.g. app/models/user.rb)
       # TODO: Why 'omniauth.auth' is nil?
       @user = User.from_omniauth(request.env["omniauth.auth"])
-
       if @user.persisted?
         flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
         sign_in_and_redirect @user, event: :authentication
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,13 +1,25 @@
 require 'omniauth-google-oauth2'
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'],
-      {
-        scope: 'email, profile',
-        prompt: 'select_account consent',
-        redirect_uri: 'http://fffff.knup.co.kr/users/auth/google_oauth2/callback',
-        image_size: 200,
-        image_aspect_ratio: 'square',
-         
-      }
+  if Rails.env.production?
+    provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'],
+        {
+          scope: 'email, profile',
+          prompt: 'select_account consent',
+          redirect_uri: 'http://fffff.knup.co.kr/users/auth/google_oauth2/callback',
+          image_size: 200,
+          image_aspect_ratio: 'square',
+           
+        }
+  else
+    provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'],
+        {
+          scope: 'email, profile',
+          prompt: 'select_account consent',
+          redirect_uri: 'https://c17a6a5bbf6541afb0540ace72f4dc08.vfs.cloud9.ap-southeast-1.amazonaws.com/users/auth/google_oauth2/callback',
+          image_size: 200,
+          image_aspect_ratio: 'square',
+           
+        }
+  end
 end
 OmniAuth.config.full_host = Rails.env.production? ? 'http://fffff.knup.co.kr/' : 'https://c17a6a5bbf6541afb0540ace72f4dc08.vfs.cloud9.ap-southeast-1.amazonaws.com/'
